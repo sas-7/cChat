c6f1456d7f290640b3546b2459ae832be6b21c99
Saurabh Singh <131347866+sas-7@users.noreply.github.com>
2023-10-25 16:54:59 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,8 +25,8 @@ const Login = () => {
         <span className="logo">cChat</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
           <button>Sign in</button>
           {err && <span>Something went wrong</span>}
         </form>
