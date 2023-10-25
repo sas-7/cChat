b056a1240e2df6ccd9dc3e7624d60bcadae603c5
Saurabh Singh <131347866+sas-7@users.noreply.github.com>
2023-10-25 16:53:23 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -65,9 +65,9 @@ const Register = () => {
         <span className="logo">cChat</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
-          <input required type="text" placeholder="display name" />
-          <input required type="email" placeholder="email" />
-          <input required type="password" placeholder="password" />
+          <input required type="text" placeholder="Name" />
+          <input required type="email" placeholder="Email" />
+          <input required type="password" placeholder="Password" />
           <input required style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img src={Add} alt="" />
